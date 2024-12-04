848992ee561326b21991b39ad42c17ce4097fbf7
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2024-12-04 11:26:22 +0200

    Adds ability to read properties from .env file

--- src/main.go
+++ src/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	controller "beautyproject/services/src/controllers"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := gin.Default()
 	controller.RegisterUserController(r)
 	r.Run()
